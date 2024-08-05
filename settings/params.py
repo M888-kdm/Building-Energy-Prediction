c923f09f7c93a3f59a606980069aafdd8944aead
Moussa Niang <niangm@ept.sn>
2024-08-05 23:25:19 +0000

    Corrected mistake on train-test splitting

--- settings/params.py
+++ settings/params.py
@@ -7,7 +7,7 @@ MODEL_PARAMS = {
     "LOG_TRANSFORM": ['LotFrontage', 'LotArea', 'GarageArea', 'GrLivArea', '1stFlrSF', 'BsmtUnfSF', 'TotalBsmtSF'],
     'DEFAULT_FEATURE_NAMES': ['Alley','BsmtQual','ExterQual','Foundation','FullBath','GarageArea','GarageCars','GarageFinish','GarageType','GrLivArea','KitchenQualMSSubClass','Neighborhood','OverallQual','TotRmsAbvGrd','building_age','remodel_age','garage_age'],
     'TEST_SIZE': 0.20,
-    "MIN_PPS": 0.1,
+    "MIN_PPS": 0.05,
     "SEED": 42
 }
 SEED = 42
