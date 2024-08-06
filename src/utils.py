5b943a47dc5e507b5b0161c566bc857a9b982e9a
Abk-coder <dkhadre@ept.sn>
2024-08-06 09:56:59 +0000

    athentication ok

--- src/utils.py
+++ src/utils.py
@@ -3,15 +3,15 @@ from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
 
-load_dotenv()
-
-os.environ["AZURE_TENANT_ID"] = os.getenv("AZURE_TENANT_ID")
-os.environ["AZURE_CLIENT_ID"] = os.getenv("AZURE_CLIENT_ID")
-os.environ["AZURE_CLIENT_SECRET"] = os.getenv("AZURE_CLIENT_SECRET")
-# Enter details of your AzureML workspace
-subscription_id = os.getenv("SUBSCRIPTION_ID")
-resource_group = os.getenv("RESOURCE_GROUP")
-workspace_name = os.getenv("AML_WORKSPACE_NAME")
+# load_dotenv()
+#
+# os.environ["AZURE_TENANT_ID"] = os.getenv("AZURE_TENANT_ID")
+# os.environ["AZURE_CLIENT_ID"] = os.getenv("AZURE_CLIENT_ID")
+# os.environ["AZURE_CLIENT_SECRET"] = os.getenv("AZURE_CLIENT_SECRET")
+# # Enter details of your AzureML workspace
+# subscription_id = os.getenv("SUBSCRIPTION_ID")
+# resource_group = os.getenv("RESOURCE_GROUP")
+# workspace_name = os.getenv("AML_WORKSPACE_NAME")
 
 
 def get_mlflow_tracking_uri():
@@ -19,6 +19,14 @@ def get_mlflow_tracking_uri():
     Configure and Get Access to Azure workspace
     :return: mlflow_tracking_uri
     """
+    load_dotenv()
+
+    # os.environ["AZURE_TENANT_ID"] = os.getenv("AZURE_TENANT_ID")
+    # os.environ["AZURE_CLIENT_ID"] = os.getenv("AZURE_CLIENT_ID")
+    # os.environ["AZURE_CLIENT_SECRET"] = os.getenv("AZURE_CLIENT_SECRET")
+    subscription_id = os.getenv("SUBSCRIPTION_ID")
+    resource_group = os.getenv("RESOURCE_GROUP")
+    workspace_name = os.getenv("AML_WORKSPACE_NAME")
     token_credential = DefaultAzureCredential()
     ml_client = MLClient(credential=token_credential,
                          subscription_id=subscription_id,
@@ -29,7 +37,6 @@ def get_mlflow_tracking_uri():
 
 
 if __name__ == "__main__":
-    print(workspace_name)
     mlflow_trackingUri = get_mlflow_tracking_uri()
     print("mlfow tracking url")
     print(mlflow_trackingUri)
