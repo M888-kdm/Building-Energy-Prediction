cc65164a1603a89f5ded56608b9eca9b9660f35b
Moussa Niang <niangm@ept.sn>
2024-08-07 05:57:10 +0000

    Added BetterStack logging

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
-import pendulum
 import os
+import pendulum
+import sys
+
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
+from logtail import LogtailHandler
+from loguru import logger
 
 def get_current_date():
     return pendulum.now().strftime('%Y%m%d_%H%m%S')
@@ -19,6 +23,12 @@ def add_prefix_to_keys(dict_, prefix):
   """
 
   return {prefix + key: value for key, value in dict_.items()}
+
+def configure_logger():
+    load_dotenv()
+    logtail_handler = LogtailHandler(source_token=os.getenv("BETTER_STACK_SOURCE_TOKEN"))
+    log_fmt = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS!UTC}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - {message}"
+    logger.configure(handlers=[{"sink": sys.stderr, "format": log_fmt}, {"sink": logtail_handler , "format": log_fmt, "level": "INFO"}])
 
 
 def get_mlflow_tracking_uri():
@@ -37,9 +47,3 @@ def get_mlflow_tracking_uri():
                          workspace_name=workspace_name)
     mlflow_tracking_uri = ml_client.workspaces.get(ml_client.workspace_name).mlflow_tracking_uri
     return mlflow_tracking_uri
-
-
-if __name__ == "__main__":
-    mlflow_trackingUri = get_mlflow_tracking_uri()
-    print("mlfow tracking url")
-    print(mlflow_trackingUri)
