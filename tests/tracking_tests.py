0197178c65acc3ed153ec9409753710cb161b4f3
Moussa Niang <niangm@ept.sn>
2024-08-07 17:34:05 +0000

    Added tests in pipeline

--- tests/tracking_tests.py
+++ tests/tracking_tests.py
@@ -1,38 +1,6 @@
 import pandas as pd
 from unittest.mock import MagicMock, patch
-from src.tracking import mlflow_log_search, find_best_run_id_by_name, get_experiment_id
-
-# def test_mlflow_log_search():
-#     """Test logging of GridSearchCV results to MLflow."""
-#     search = MagicMock()
-#     search.best_params_ = {'param1': 1, 'param2': 2}
-#     search.best_score_ = 0.9
-#     search.best_estimator_ = MagicMock()
-
-#     with patch("mlflow.log_params") as mock_log_params, \
-#          patch("mlflow.log_metric") as mock_log_metric, \
-#          patch("mlflow.sklearn.log_model") as mock_log_model:
-#         mlflow_log_search(search)
-
-#         mock_log_params.assert_called_once_with({'param1': 1, 'param2': 2})
-#         mock_log_metric.assert_called_once_with("best_score", 0.9)
-#         mock_log_model.assert_called_once_with(search.best_estimator_, "model")
-
-# def test_find_best_run_id_by_name():
-#     """Test finding the run ID with the best R2 score."""
-#     experiment_name = "building-energy-prediction-evaluation"
-#     mock_experiment = MagicMock()
-#     mock_experiment.experiment_id = "1234"
-
-#     mock_runs = pd.DataFrame({
-#         'run_id': ['run1', 'run2'],
-#         'metrics.test_r2': [0.8, 0.9]
-#     })
-
-#     with patch("mlflow.get_experiment_by_name", return_value=mock_experiment) as mock_get_experiment_by_name, \
-#          patch("mlflow.search_runs", return_value=mock_runs) as mock_search_runs:
-#         best_run_id = find_best_run_id_by_name()
-#         assert best_run_id == "run2", "The best run ID is not correctly identified."
+from src.tracking import get_experiment_id
 
 def test_get_experiment_id():
     """Test retrieving or creating an experiment ID."""
--- tests/tuning_tests.py
+++ tests/tuning_tests.py
@@ -5,39 +5,6 @@ from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 from src.tuning import fine_tune_models, rmse
 
-# @patch('src.tuning.mlflow_log_search')
-# def test_fine_tune_models(mock_mlflow_log_search):
-#     # Setup mock return values
-#     mock_mlflow_log_search.return_value = {"param1": 0.1, "param2": 0.01}
-
-#     # Define the estimator_params with estimators and parameter grids
-#     estimator_params = {
-#         "LinearRegression": {
-#             "estimator": LinearRegression(),
-#             "params": {
-#                 "estimator__regressor__fit_intercept": [True, False]
-#             }
-#         },
-#         "RandomForest": {
-#             "estimator": RandomForestRegressor(),
-#             "params": {
-#                 "estimator__regressor__n_estimators": [10, 50, 100],
-#                 "estimator__regressor__max_depth": [None, 10, 20]
-#             }
-#         }
-#     }
-
-#     # Create synthetic data for testing
-#     x_train = pd.DataFrame(np.random.randn(100, 2), columns=["feature1", "feature2"])
-#     y_train = pd.Series(np.random.randn(100))
-
-#     # Call the function to be tested
-#     result = fine_tune_models(estimator_params, x_train, y_train)
-
-#     # Assertions and other test logic here
-#     assert "LinearRegression" in result, "LinearRegression model should be present in the result."
-#     assert "RandomForest" in result, "RandomForest model should be present in the result."
-    
 def test_rmse():
     """Test the RMSE calculation function."""
     y_actual = np.array([1.0, 2.0, 3.0])
