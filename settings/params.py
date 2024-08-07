3af71cca07316cf94632156dcfa45a7a4eddf4fe
Moussa Niang <niangm@ept.sn>
2024-08-07 20:04:42 +0000

    added outputs

--- settings/params.py
+++ settings/params.py
@@ -1,6 +1,10 @@
 """Project parameters"""
 
 import os
+import dotenv
+
+dotenv.load_dotenv()
+
 MODEL_PARAMS = {
     "MIN_COMPLETION_RATE": 0.75,
     "TARGET_NAME": "SiteEnergyUse(kBtu)",
@@ -11,7 +15,11 @@ MODEL_PARAMS = {
     "SEED": 42
 }
 SEED = 42
+
 DATASETS_DIR = "datasets"
-NOTEBOOKS_DIR = "notebooks"
+
+if(os.path.basename(os.getcwd()) == "notebooks"):
+    DATASETS_DIR = "../datasets"
+
 RAW_DATA=os.path.join(DATASETS_DIR, "raw_data.csv")
 CLEANED_DATA=os.path.join(DATASETS_DIR, "cleaned_data.csv")
