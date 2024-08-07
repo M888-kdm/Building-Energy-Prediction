d2841c143d954d88247ebf3c70c1ade1c8032b96
Moussa Niang <niangm@ept.sn>
2024-08-07 12:23:30 +0000

    Bug fixes

--- src/deploy.py
+++ src/deploy.py
@@ -2,7 +2,7 @@ import mlflow
 from azure_ml import deploy_model_to_azure_ml_endpoint, get_mlflow_tracking_uri
 
 def deploy():
-    uri = get_mlflow_tracking_uri
+    uri = get_mlflow_tracking_uri()
     mlflow.set_tracking_uri(uri)
     deploy_model_to_azure_ml_endpoint()
 
