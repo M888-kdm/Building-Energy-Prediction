3ae076e67b8878c55f3cac09798f1b2a05cc62ba
Moussa Niang <niangm@ept.sn>
2024-08-07 12:16:57 +0000

    Fixes on workflow

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import papermill as pm
+import sys
+
+from pathlib import Path
+from utils import create_dir_if_not_exists
+
+notebooks = [
+  '01-data-collection.ipynb',
+  '02-exploratory-data-analysis.ipynb',
+  '03-data-preparation.ipynb',
+  '04-modelisation.ipynb'
+]
+
+# Check if output dir exists and create it if not
+dir_to_create = "notebooks/outputs"
+create_dir_if_not_exists(dir_to_create)
+
+for notebook in notebooks:
+    notebook_path = f"notebooks/{notebook}"
+    output_notebook = f"{'notebooks/outputs'}/{notebook.replace('.ipynb', '_output.ipynb')}"
+    pm.execute_notebook(notebook_path, output_notebook)
